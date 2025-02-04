3b24b3319d53ac6c0f101505214686f4e91f9e4c
陳冠蓉 <82246606+AnnaChen1228@users.noreply.github.com>
2025-02-04 23:46:09 +0800

    [Fix] astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,15 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [rehypeAccessibleEmojis],
   },
-  site: 'https://annachen1228.github.io', // 移除 /blog
+  site: 'https://annachen1228.github.io',
   base: '/blog',
+  outDir: './dist', // 明確指定輸出目錄
+  trailingSlash: 'always', // 確保 URL 結尾有斜線
   output: 'static',
   integrations: [tailwind(), react(), robotsTxt(), sitemap()],
+  build: {
+    assets: '_assets' // 指定資產目錄
+  },
   server: {
     host: '0.0.0.0',
     port: 4321
